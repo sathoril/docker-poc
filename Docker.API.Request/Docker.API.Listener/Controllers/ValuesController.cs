df477357e916f095973e626669a07946bbcf39ae
Bruno Fernando <bfcorrea@hcor.com.br>
2019-11-22 16:53:23 -0300

    Finished initial implementation

---
+++ Docker.API.Request/Docker.API.Listener/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Docker.API.Listener.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            Random random = new Random();
+            return $"Hello World from ValuesController - {random.Next(1,100)}";
+        }
+    }
+}
---
+++ Docker.API.Request/Docker.API.Request/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Docker.API.Request.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            //string url = "http://localhost:8082/api/values"; // sample url
+             string url = "http://docker.api.listener/api/values"; // sample url
+
+            using (HttpClient client = new HttpClient())
+            {
+                return await client.GetStringAsync(url);
+            }
+        }
+    }
+}
